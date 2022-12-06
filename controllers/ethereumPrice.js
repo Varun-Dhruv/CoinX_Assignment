f9593c4c6a113d9d21a2e27a45cc3b5f89b3ff33
Varun Koranne <88835482+Varun-Dhruv@users.noreply.github.com>
2022-12-06 21:56:47 +0530

    Added comments

--- controllers/ethereumPrice.js
+++ controllers/ethereumPrice.js
@@ -1,17 +1,17 @@
 const axios = require("axios");
 const { ethereumDetails } = require("../models/ethereumPrice");
+
 const getEthereumPrice = async () => {
   try {
     const { data } = await axios.get(
+      // Fetching the current Ethereum price from the given endpoint
       "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&amp;vs_currencies=inr"
     );
-    const ethereumValueJson = JSON.parse(data.slice(3, 30));
-    console.log(ethereumValueJson.ethereum.inr);
+    const ethereumValueJson = JSON.parse(data.slice(3, 30)); // cleaning the result obtained to remove redundant symbols and converting into JSON object
     await new ethereumDetails({
       ethereumPrice: ethereumValueJson.ethereum.inr,
       currency: "inr",
-    }).save();
-    //res.status(200).send(ethereumValueJson);
+    }).save(); // saving the current price of ethereum into the database.
   } catch (error) {
     console.error(error);
     // res.status(500).send({ message: "Internal Server Error" });
@@ -19,7 +19,7 @@ const getEthereumPrice = async () => {
 };
 const fetchEthereumDetailsfromDB = async () => {
   try {
-    const data = await ethereumDetails.find().sort({ _id: -1 }).limit(1);
+    const data = await ethereumDetails.find().sort({ _id: -1 }).limit(1); //fetching the latest price of ethereum from database.
     
     if (data) return data[0].ethereumPrice;
   } catch (error) {
--- controllers/transaction.js
+++ controllers/transaction.js
@@ -4,10 +4,11 @@ const { fetchEthereumDetailsfromDB } = require("./ethereumPrice");
 
 const getTransactions = async (req, res, next) => {
   try {
-    if (req.params.address) var address = req.params.address;
+    if (req.params.address) var address = req.params.address; // if the user provides a address use that account address to fetch details
     else var address = "0xce94e5621a5f7068253c42558c147480f38b5e0d";
-    const result = await axios.get(
-      "http://api.etherscan.io/api?module=account&action=txlist" +
+    // API call to ether scan to fetch the list of normal transactions for given account address.
+    const result = await axios.get(                               
+      "http://api.etherscan.io/api?module=account&action=txlist" +  
         "&address=" +
         address +
         "&startblock=0" +
@@ -23,13 +24,14 @@ const getTransactions = async (req, res, next) => {
         },
       }
     );
-    const data = result.data.result;
-    const { error } = validateTransaction(data);
+    const data = result.data.result;  //extract the data from result
+    const { error } = validateTransaction(data);  //validate the schema of Transaction using Joi
     if (error)
-      return res.status(422).send({ message: "Invalid  format", error: error });
-    await transactions.insertMany(data);
+      return res.status(422).send({ message: "Invalid  format", error: error }); // if the schema is invalid through an 422 Exception
+
+    await transactions.insertMany(data);  // Insert into database the list of transactions
     // await transactions(data).save();
-    return res.status(200).send(data);
+    return res.status(200).send(data);  // return the list of transactions to the user.
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "Internal Server Error" });
@@ -37,19 +39,19 @@ const getTransactions = async (req, res, next) => {
 };
 const getAccountBalance = async (req, res, next) => {
   try {
-    const transactionsList = await transactions.find({});
-    var balance = 0;
-    transactionsList.map((transaction) => {
-      if (transaction.from === req.params.address) {
+    const transactionsList = await transactions.find({}); // fetching a list of all transactions from the database
+    var balance = 0; // initial balance
+    transactionsList.map((transaction) => {  // traversing through the transaction list array 
+      if (transaction.from === req.params.address) { // if the transaction is carried by the user then deduct the value of transaction from account
         balance -= Number(transaction.value);
-      } else if (transaction.to === req.params.address) {
+      } else if (transaction.to === req.params.address) {  // if the transaction is to the users account  add the value of transaction to his balance 
         balance += Number(transaction.value);
       }
     });
-    const ethereumValue = await fetchEthereumDetailsfromDB();
+    const ethereumValue = await fetchEthereumDetailsfromDB(); // get the current value of ethereum from database
     return res
       .status(200)
-      .send({ etherValue: balance, priceInINR: balance / ethereumValue });
+      .send({ etherValue: balance, priceInINR: balance / ethereumValue ,currentPriceofEthereum:ethereumValue});  // returning the required data
   } catch (error) {
     console.error(error);
   }
